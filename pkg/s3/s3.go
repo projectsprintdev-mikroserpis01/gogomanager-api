182990ef2231debb829ec1eb3b0e96e730617d3b
ahargunyllib <billy.bpm03@gmail.com>
2025-01-09 23:53:00 +0700

    feat: enhance S3 upload functionality with input validation and unique file naming

--- pkg/s3/s3.go
+++ pkg/s3/s3.go
@@ -1,7 +1,10 @@
 package s3
 
 import (
+	"errors"
+	"fmt"
 	"mime/multipart"
+	"time"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/credentials"
@@ -23,14 +26,6 @@ type S3Struct struct {
 var S3 = getS3()
 
 func getS3() S3Interface {
-	log.Info(log.LogInfo{
-		"message": "Creating new S3 session",
-		"region":  env.AppEnv.AWSRegion,
-		"bucket":  env.AppEnv.AWSS3BucketName,
-		"access":  env.AppEnv.AWSAccessKeyID,
-		"secret":  env.AppEnv.AWSSecretAccessKey,
-	}, "[S3][getS3] creating new S3 session")
-
 	session := session.Must(session.NewSession(&aws.Config{
 		Region: aws.String(env.AppEnv.AWSRegion),
 		Credentials: credentials.NewStaticCredentials(
@@ -49,43 +44,56 @@ func getS3() S3Interface {
 }
 
 func (s *S3Struct) Upload(file *multipart.FileHeader) (string, error) {
+	// Validate input
+	if file == nil {
+		err := errors.New("file is nil")
+		log.Error(log.LogInfo{
+			"error": err.Error(),
+		}, "[S3][Upload] invalid input")
+		return "", err
+	}
+
+	// Open file
 	fileContent, err := file.Open()
 	if err != nil {
 		log.Error(log.LogInfo{
 			"error": err.Error(),
 		}, "[S3][Upload] failed to open file")
-
 		return "", err
 	}
+	defer func() {
+		if cerr := fileContent.Close(); cerr != nil {
+			log.Warn(log.LogInfo{
+				"error": cerr.Error(),
+			}, "[S3][Upload] failed to close file")
+		}
+	}()
 
-	defer fileContent.Close()
+	// Generate unique file name
+	timeNow := time.Now().Unix()
+	fileName := fmt.Sprintf("%d-%s", timeNow, file.Filename)
 
-	fileName := file.Filename
-
-	val, err := s.session.Config.Credentials.Get()
-	if err != nil {
-		log.Error(log.LogInfo{
-			"error": err.Error(),
-		}, "[S3][Upload] failed to get credentials")
+	// Determine content type with fallback
+	contentType := file.Header.Get("Content-Type")
+	if contentType == "" {
+		contentType = "application/octet-stream" // Fallback for unknown file types
 	}
 
-	log.Info(log.LogInfo{
-		"access": val.AccessKeyID,
-		"secret": val.SecretAccessKey,
-		"token":  val.SessionToken,
-		"provider":   val.ProviderName,
-	}, "[S3][Upload] uploading file")
-
+	// Upload to S3
 	result, err := s.uploader.Upload(&s3manager.UploadInput{
-		Bucket: aws.String(env.AppEnv.AWSS3BucketName),
-		Key:    aws.String(fileName),
-		Body:   fileContent,
+		Bucket:      aws.String(env.AppEnv.AWSS3BucketName),
+		Key:         aws.String(fileName),
+		Body:        fileContent,
+		ACL:         aws.String("public-read"),
+		ContentType: aws.String(contentType),
 	})
 	if err != nil {
 		log.Error(log.LogInfo{
 			"error": err.Error(),
 		}, "[S3][Upload] failed to upload file")
+		return "", err
 	}
 
+	// Return public URL
 	return result.Location, nil
 }
