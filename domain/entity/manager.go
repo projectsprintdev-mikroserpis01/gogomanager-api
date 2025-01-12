bed15b74c71ab91457e933b712131235cfd8ab55
ahargunyllib <billy.bpm03@gmail.com>
2025-01-13 02:45:11 +0700

    fix: change manager fields to pointers for optional values

--- domain/entity/manager.go
+++ domain/entity/manager.go
@@ -6,9 +6,9 @@ type Manager struct {
 	ID              int       `db:"id"`
 	Email           string    `db:"email"`
 	Password        string    `db:"password"`
-	Name            string    `db:"name"`
-	UserImageURI    string    `db:"user_image_uri"`
-	CompanyName     string    `db:"company_name"`
-	CompanyImageURI string    `db:"company_image_uri"`
+	Name            *string   `db:"name"`
+	UserImageURI    *string   `db:"user_image_uri"`
+	CompanyName     *string   `db:"company_name"`
+	CompanyImageURI *string   `db:"company_image_uri"`
 	CreatedAt       time.Time `db:"created_at"`
 }
